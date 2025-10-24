e600a4ed9f0b674a529526a1959b85efd870c9a2
Ahmed Nasr <https://github.com/nasr-analyst>
2025-10-24 10:52:33 +0300

    Updated homepage design and added icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-console.log("Test file for main.js");
+console.log("Category Filter Activated");
+
+
+let category_btns = document.querySelectorAll('.category');
+
+
+let products = document.querySelectorAll('article.product');
+
+
+category_btns.forEach(btn => {
+    btn.onclick = function() {
+        
+        let selectedId = btn.id;
+
+        category_btns.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+
+    //    كل المنتجات
+        if (selectedId == '0') {
+            products.forEach(prod => prod.style.display = 'block');
+        }
+        
+        else {
+            products.forEach(prod => {
+                if (prod.id == selectedId) {
+                    prod.style.display = 'block';
+                } else {
+                    prod.style.display = 'none';
+                }
+            });
+        }
+    };
+});
